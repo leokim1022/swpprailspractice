b2c9f9e86cd7046218afd92dbbeab61920bcae3a
root <admin@example.com>
2015-04-06 19:54:47 +0900

    submit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,17 @@
 class ArticlesController < ApplicationController
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
 	def index
 		@articles = Article.all
 	end
